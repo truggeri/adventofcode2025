1f87d730414260a71458c07137dbe94818685665
Thomas Ruggeri <truggeri@github.com>
2025-12-09 17:45:30 +0000

    chore: Day1 refactor for readability

--- lib/day1.rb
+++ lib/day1.rb
@@ -4,6 +4,9 @@
 module Day1
   module_function
 
+  DIAL_SIZE = 100
+  LEFT = 'L'
+
   def run
     input = read_input
     puts "Part 1: #{part1(input)}"
@@ -11,20 +14,36 @@ module Day1
   end
 
   def part1(input)
+    count_zeros(input, count_during_rotation: false)
+  end
+
+  def part2(input)
+    count_zeros(input, count_during_rotation: true)
+  end
+
+  def count_zeros(input, count_during_rotation:)
     rotations = parse_rotations(input)
     position = 50
     zero_count = 0
 
     rotations.each do |direction, distance|
-      if direction == 'L'
-        position = (position - distance) % 100
-      else # direction == 'R'
-        position = (position + distance) % 100
+      new_position = rotate(position, direction, distance)
+      
+      if count_during_rotation
+        zero_count += count_zeros_in_range(position, distance, direction)
+      else
+        zero_count += 1 if new_position == 0
       end
-      zero_count += 1 if position == 0
+      
+      position = new_position
     end
 
     zero_count
+  end
+
+  def rotate(position, direction, distance)
+    move_down = direction == LEFT ? -1 : 1
+    (position + move_down * distance) % DIAL_SIZE
   end
 
   def parse_rotations(input)
@@ -35,40 +54,15 @@ module Day1
     end
   end
 
-  def part2(input)
-    rotations = parse_rotations(input)
-    position = 50
-    zero_count = 0
-
-    rotations.each do |direction, distance|
-      # Count how many times we pass through 0 during this rotation
-      if direction == 'L'
-        # Moving left (decreasing)
-        new_position = (position - distance) % 100
-        # Count complete wraps plus any crossing of 0
-        zero_count += count_zeros_in_range(position, new_position, distance, :left)
-        position = new_position
-      else # direction == 'R'
-        # Moving right (increasing)
-        new_position = (position + distance) % 100
-        # Count complete wraps plus any crossing of 0
-        zero_count += count_zeros_in_range(position, new_position, distance, :right)
-        position = new_position
-      end
-    end
-
-    zero_count
-  end
-
-  def count_zeros_in_range(start_pos, end_pos, distance, direction)
-    complete_loops = distance / 100
+  def count_zeros_in_range(start_pos, distance, direction)
+    complete_loops = distance / DIAL_SIZE
     zero_count = complete_loops
-    remaining = distance % 100
+    remaining = distance % DIAL_SIZE
     
-    if direction == :left
+    if direction == LEFT
       zero_count += 1 if start_pos.positive? && (start_pos - remaining) <= 0
     else # :right
-      zero_count += 1 if (start_pos + remaining) >= 100
+      zero_count += 1 if (start_pos + remaining) >= DIAL_SIZE
     end
 
     zero_count
